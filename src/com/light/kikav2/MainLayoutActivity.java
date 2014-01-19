85c55b5dc222b4ca274bd2a82f98cdbfddf4bd4a
N4gato <bouayad.n4@gmail.com>
2014-01-19 08:37:05 +0000

    somthing

--- src/com/light/kikav2/MainLayoutActivity.java
+++ src/com/light/kikav2/MainLayoutActivity.java
@@ -3,13 +3,19 @@ package com.light.kikav2;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainLayoutActivity extends Activity {
 
+	
+	TextView tv1;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_layout);
+		tv1 = (TextView) findViewById(R.id.tv1);
+		tv1.setText("Working");
 	}
 
 	@Override
